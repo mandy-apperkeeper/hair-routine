052fd01072ce32bfc1a15f6d5acb16f5cea9e870
admiralmcsassy <60167091+admiralmcsassy@users.noreply.github.com>
2026-05-10 14:43:25 -0400

    Bump SW cache to v4 to force refresh of stale cached pages

--- hair-sw.js
+++ hair-sw.js
@@ -12,7 +12,7 @@
  * - Posts 'SW_UPDATED' message to all clients so UI can show "Updated" indicator
  */
 
-var CACHE_VERSION = 'v3';
+var CACHE_VERSION = 'v4';
 var CACHE_NAME = 'hair-routine-cache-' + CACHE_VERSION;
 
 var PRECACHE_URLS = [
